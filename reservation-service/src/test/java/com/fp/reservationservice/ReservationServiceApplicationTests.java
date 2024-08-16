8a73e4681739a945cfcef614be2fc93061d1acd4
Mungi Cheon <c.mungi7421@gmail.com>
2024-08-16 12:25:05 +0900

    feat : #FINAL-1 init project
    - 프로젝트 초기화
      - 멀티 모듈 생성
      - Spring cloud config 서버 설정

---
+++ reservation-service/src/test/java/com/fp/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fp.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ review-service/src/test/java/com/fp/reviewservice/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fp.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
